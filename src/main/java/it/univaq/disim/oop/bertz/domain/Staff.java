bbdc7be495ebcab6ae7fe3fd6bc2bc8c5d5423b3
christianfe <christian.felicione@gmail.com>
2021-08-12 23:07:23 +0200

    cleaning

--- src/main/java/it/univaq/disim/oop/bertz/domain/Staff.java
+++
@@ -1,7 +0,0 @@
-package it.univaq.disim.oop.bertz.domain;
-
-public class Staff extends User {
-	
-	
-
-}
--- src/main/java/it/univaq/disim/oop/bertz/domain/VeicleState.java
+++
@@ -1,5 +0,0 @@
-package it.univaq.disim.oop.bertz.domain;
-
-public enum VeicleState {
-	FREE, BUSY, MAINTENANCE;
-}
