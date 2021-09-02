0a355e195966acdc081d98d875a2cb96a06bc3df
christianfe <christian.felicione@gmail.com>
2021-09-02 18:42:43 +0200

    eh gia

--- src/main/java/it/univaq/disim/oop/bhertz/controller/TypeController.java
+++ src/main/java/it/univaq/disim/oop/bhertz/controller/TypeController.java
@@ -1,8 +1,5 @@
 package it.univaq.disim.oop.bhertz.controller;
 
-import java.awt.AWTException;
-import java.awt.Robot;
-import java.awt.event.InputEvent;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -10,7 +7,6 @@ import java.util.ResourceBundle;
 import it.univaq.disim.oop.bhertz.business.BusinessException;
 import it.univaq.disim.oop.bhertz.business.TypesService;
 import it.univaq.disim.oop.bhertz.business.impl.ram.RAMTypesServiceImpl;
-import it.univaq.disim.oop.bhertz.domain.Contract;
 import it.univaq.disim.oop.bhertz.domain.Type;
 import it.univaq.disim.oop.bhertz.domain.User;
 import it.univaq.disim.oop.bhertz.view.ViewDispatcher;
@@ -21,8 +17,6 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
@@ -67,6 +61,7 @@ public class TypeController implements Initializable, DataInitializable<User> {
 		hoursColumn.setCellValueFactory((CellDataFeatures<Type, String> param) -> {
 			return new SimpleStringProperty(param.getValue().getPriceForDay() + " €/h");
 		});
+
 		kmColumn.setCellValueFactory((CellDataFeatures<Type, String> param) -> {
 			return new SimpleStringProperty(param.getValue().getPriceForKm() + " €/km");
 		});
@@ -101,20 +96,5 @@ public class TypeController implements Initializable, DataInitializable<User> {
 			dispatcher.renderError(e);
 		}
 		this.user = user;
-
-		/*
-		MenuItem menuView = new MenuItem("Visualizza Veicoli");
-		MenuItem menuEdit = new MenuItem("Modifica Tipologia");
-		MenuItem menuDelete = new MenuItem("Elimina Tipologia");
-		menuView.setOnAction((ActionEvent event) -> {
-		    Object param = typesTable.getSelectionModel().getSelectedItem();
-		    dispatcher.renderView("veicles", param);
-		});
-		localMenuButton.getItems().add(menuView);
-		if (user.getRole() == 0) {
-			menu.getItems().add(menuEdit);
-			menu.getItems().add(menuDelete);
-		}
-		typesTable.setContextMenu(menu);*/
 	}
 }
